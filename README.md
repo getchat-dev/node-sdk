6d8ccd56d588680918e5b7ae1f4a1b6ee5bc1f51
Evgeniy Tyurin <et@markuper.om>
2021-05-26 09:58:12 +0300

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # emby-node-sdk
 EMBY SDK for Node
+
+## Installation
+
+    npm install @emby-chat/node-sdk
+or
+
+    yarn add @emby-chat/node-sdk
+
+## Init
+
+    const Emby = require('@emby-chat/node-sdk');
+
+    const emby = new Emby({
+        id: 'your id',
+        secret: 'your secret key',
+        api_token: 'your api token',
+        base_url: 'https://emby-chat.io/'
+    });
+
+## Methods
+
+### urlByChatId
+generate chat url by chat id
+parameters
+1. required (string) chat_id
+2. (object) user
+3. (array[object]) recipients
+4. (object) extra - options
+
+generate chat url for unauth user
+
+    emby.urlByChatId('user10');
+
+generate chat url for auth user
+
+    emby.urlByChatId('user10', {id: '10', name: 'User Name'});
+
+generate chat url for auth user with avatar
+
+    emby.urlByChatId('user10', {id: '10', name: 'User Name', avatar: 'source to avatar'});
+
+generate chat url for auth user with options
+
+    emby.urlByChatId('user10', {id: '10', name: 'User Name'}, [], {
+        'skin': 'default',
+        'skin_options': {
+            'hideDeletedMessage' => true
+        }
+    });
---
+++ index.js
@@ -0,0 +1,116 @@
+const crypto = require('crypto');
+
+const strRandom = function(len = 10) {
+
+    let text = '';
+
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for(let i = 0; i < len; i++)
+    {
+        text+= charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+
+    return text;
+}
+
+const urlQueryBuild = (scope = '', params) => {
+    const scopePrefix = (key, value) => {
+        return scope ? `${scope}[${key}]=${value}` : `${key ? `${key}=` : ''}${value}`;
+    };
+
+    const query =
+        Object.keys(params)
+            .map(key => {
+                if(params[key] && typeof(params[key]) === 'object') {
+                    return scopePrefix('', urlQueryBuild(key, params[key]));
+                }
+                else {
+                    return scopePrefix(key, params[key]);
+                }
+            })
+            .join('&');
+
+    return query;
+}
+
+class Emby {
+
+    constructor(config = {}) {
+        this.clientId = config.id;
+        this.clientSecret = config.secret;
+        this.apiToken = config.api_token;
+        this.baseUrl = config.base_url;
+    }
+
+    /**
+     * @param chatId
+     * @param Array user
+     * @param Array[] recipients
+     * @param Array extra
+     * @return string
+     */
+     urlByChatId(chatId, user = [], recipients = [], extra = {})
+     {
+        const rnd = strRandom(32);
+
+        const signatureParams = [
+            this.clientId,
+            this.clientSecret,
+            rnd
+        ];
+
+        const queryParams = {
+            'client_id': this.clientId,
+            'chat_id': chatId,
+            'rnd': rnd,
+            'user': [],
+            'recipients': []
+        };
+ 
+        if(user) {
+            if(user.id) {
+                signatureParams.push(queryParams['user']['id'] = user.id);
+            }
+
+            if(user.name) {
+                signatureParams.push(queryParams['user']['name'] = user.id);
+            }
+
+            if(user.avatar)
+            {
+                signatureParams.push(queryParams['user']['picture'] = user.avatar);
+            }
+        }
+ 
+        recipients.forEach(recipient => {
+            signatureParams.push(recipient['id']);
+            signatureParams.push(recipient['name']);
+
+            const recipientData = {
+                'id': recipient['id'],
+                'name': recipient['name'],
+                'is_bot': recipient['is_bot'] || false
+            };
+
+            if(recipient.avatar) {
+                recipientData['picture'] = recipient.avatar;
+            }
+
+            queryParams['recipients'].push(recipientData);
+        });
+
+        signatureParams.push(chatId);
+        queryParams['signature'] = crypto.createHash('md5').update(signatureParams.join(',')).digest('hex');
+
+        Object.keys(extra).forEach(key => {
+            queryParams[key] = extra[key];
+        });
+
+        const query = urlQueryBuild('', queryParams);
+
+        return `${this.baseUrl}?${query}`;
+    }
+}
+
+module.exports = Emby;
